6396cd912ca4c808c54ecd5b349f88d09e0a301d
re-gis <irumvaregisdmc@gmail.com>
2024-02-13 17:26:21 +0200

    Checkout func

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
-    digital = models.BooleanField(default=False, null=True, blank=True)
+    digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self) -> str:
--- store/views.py
+++ store/views.py
@@ -15,7 +15,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {"get_cart_items": 0, "get_cart_total": 0}
+        order = {"get_cart_items": 0, "get_cart_total": 0, "shipping": False}
         cartItems = order["get_cart_items"]
 
     products = Product.objects.all()
@@ -31,7 +31,7 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {"get_cart_items": 0, "get_cart_total": 0}
+        order = {"get_cart_items": 0, "get_cart_total": 0, "shipping": False}
         cartItems = order
 
     context = {"items": items, "order": order, "cartItems": cartItems}
